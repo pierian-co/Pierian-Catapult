38d0784bd140369f166c30314e1ec5b1de8d2011
rajneesh <rajneesh@pierian.co>
2022-03-05 17:29:14 +0000

    removed

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,13 +12,7 @@ export const Login = () => {
 
    const history = useHistory()
 
-   targetView('Login');
-   function targetView(viewName) {
-    // Validate if the Target Libraries are available on your website
-    if (window.adobe && window.adobe.target && typeof window.adobe.target.triggerView === 'function') {
-      window.adobe.target.triggerView(viewName);
-    }
-  }
+ 
     const emailChange = (e) =>{
         email_value = e.target.value
     }
